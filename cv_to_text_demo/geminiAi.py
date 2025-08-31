efaa46fb98279971ec6d8d0ac0b75b5d5acee36b
SRIJAN_RAY <srijanray11@gmail.com>
2025-08-31 21:08:34 +0530

    first commit by srijan ray

---
+++ cv_to_text_demo/geminiAi.py
@@ -0,0 +1,32 @@
+import os
+import google.generativeai as genai
+import dotenv
+
+''' this in the Era_assistant function working explanation
+    first we load the environment variables from the .env file
+    then we set the api_key of the gemini-Ai model
+    then we configure this model to use the loaded api_key
+    then we create an instance of the model
+    on this model we send the massage or task to the model
+    and return the response of the model
+'''
+dotenv.load_dotenv()
+api=os.getenv('Api_key')
+
+def Era_assistant(Task : str | None)-> str:
+    if Task is None:
+        return
+    try:
+        genai.configure(api_key=api)
+    except KeyError:
+        return "Error: Api not working or Api key not set."
+    model = genai.GenerativeModel('gemini-2.5-flash')
+    
+    try:
+        response = model.generate_content(Task)
+        return f"{response.text}"
+    except Exception as e:
+        return f"Error: {e}"
+
+# if __name__=="__main__":
+#     print(Era_assistant("what is Ai?"),"\nthis is the msg!")# remove this in production
