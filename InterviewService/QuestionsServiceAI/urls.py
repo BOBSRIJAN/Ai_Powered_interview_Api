39f2bef7faff77b8751b64f749aea87520d3d452
SRIJAN_RAY <srijanray11@gmail.com>
2025-09-29 21:24:19 +0530

    ResumeService Branch updating

--- InterviewService/QuestionsServiceAI/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('isActive/', views.isActive, name='isActive'),
-    path('PostVideos/', views.PostVideoData, name='PostVideoData'),
-]
