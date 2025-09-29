b99cd06530e212a6153530e8c5a2665a53b575c5
SRIJAN_RAY <srijanray11@gmail.com>
2025-09-29 21:39:30 +0530

    InterviewService Branch updating and readme, license added by srijan

--- ResumeService/ResumeService/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('resumeanalyzer/api/v1/', include('ResumeUploadServiceApi.urls')),
-]
--- ResumeService/ResumeUploadServiceApi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('user/', views.User, name='User'),
-    path('analyzewithdocument/', views.AnalyzeWithDocument, name='AnalyzeWithDocument'),
-    path('analyzewithjson/', views.AnalyzeWithJson, name='AnalyzeWithJson')
-]
