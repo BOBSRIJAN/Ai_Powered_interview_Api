5782721278f825685f63c2a205dd52a18d529efd
SRIJAN_RAY <srijanray11@gmail.com>
2026-01-26 17:56:46 +0530

    performance update code base change Django[DRF] to fast-Api commit by srijan

--- ResumeService/ResumeUploadServiceApi/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from . import PingTest
-
-class ResumeuploadserviceapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ResumeUploadServiceApi'
-
-    def ready(self):
-        PingTest.start()
